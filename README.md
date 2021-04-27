7f9023f74cc450751bd1f84060999975cedee38d
Wolfson Liu <wolfsonliu@live.com>
2021-04-27 21:29:48 +0800

    single sequence text box

---
+++ README.md
@@ -0,0 +1,14 @@
+# tailorbird
+
+一个简单的批量处理DNA序列的小软件。
+
+![tailorbird](tailorbird.png)
+
+可以给序列加头加尾，反向互补，大小写转换等。
+
+## 依赖
+
+* python3
+* tkinter
+
+在Windows，Mac OS，Linux环境下均可以运行。
--- tailorbird.py
+++ tailorbird.py
@@ -4,14 +4,15 @@
 # ------------------
 # Author: Wolfson Liu
 # Modified Date: 2021-04-27
-# Version: 0.1.3
+# Version: 0.1.4
 # Description:
 #    Used for multiple sequence processing in GUI.
 # ------------------
 
 import tkinter as tk
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
+
 
 def complement(sequence):
     if not isinstance(sequence, str):
@@ -23,6 +24,7 @@ def complement(sequence):
     result = ''.join([atcg.get(x, x) for x in sequence])
     return result
 
+
 def reverse_complement(sequence):
     if not isinstance(sequence, str):
         raise ValueError('Sequence should be string.')
@@ -30,8 +32,10 @@ def reverse_complement(sequence):
     result = _comp[::-1]
     return result
 
+
 class Tailorbird(tk.Frame):
     def __init__(self, master):
+        self._box_bd = 3
         super().__init__(master)
         self.versionlabel = tk.Label(
             self,
@@ -40,25 +44,13 @@ class Tailorbird(tk.Frame):
             )
         )
         self.versionlabel.grid(row=0, column=0)
-        self.in_textbox = self._textbox(
-            '序列输入框',
-            width=40, textwrap=tk.NONE,
+        self.seq_text_box = self._textbox(
+            '序列框',
+            width=80, textwrap=tk.NONE,
             row=1, column=0
         )
-        self.exchange_button = tk.Button(
-            self,
-            text='输入<=输出',
-            command=self.button_exchange
-        )
-        self.exchange_button.grid(row=1, column=1)
-        self.out_textbox = self._textbox(
-            '序列输出框',
-            width=40, textwrap=tk.NONE,
-            row=1, column=2
-        )
-        self._action_buttons(row=2, column=0, columnspan=3)
+        self._action_buttons(row=2, column=0, columnspan=1)
         self.pack(side='top')
-
 
     def _action_buttons(self, row, column, columnspan):
         self.buttons = tk.Frame(self)
@@ -67,45 +59,58 @@ class Tailorbird(tk.Frame):
         )
         self.button_dict = dict()
         self.button_dict['大写'] = tk.Button(
-            self.buttons, text='大写', command=self.button_upper
+            self.buttons, text='大写', width=8,
+            command=self.button_upper
         )
         self.button_dict['大写'].grid(row=0, column=0)
         self.button_dict['小写'] = tk.Button(
-            self.buttons, text='小写', command=self.button_lower
+            self.buttons, text='小写', width=8,
+            command=self.button_lower
         )
         self.button_dict['小写'].grid(row=0, column=1)
         self.button_dict['反向互补'] = tk.Button(
-            self.buttons, text='反向互补',
+            self.buttons, text='反向互补', width=8,
             command=self.button_reverse_complement
         )
         self.button_dict['反向互补'].grid(row=0, column=2)
         self.button_dict['反向'] = tk.Button(
-            self.buttons, text='反向', command=self.button_reverse
+            self.buttons, text='反向', width=8,
+            command=self.button_reverse
         )
         self.button_dict['反向'].grid(row=0, column=3)
         self.button_dict['互补'] = tk.Button(
-            self.buttons, text='互补', command=self.button_complement
+            self.buttons, text='互补', width=8,
+            command=self.button_complement
         )
         self.button_dict['互补'].grid(row=0, column=4)
-        self.add_head_entry = tk.Entry(
-            self.buttons, width=30
+        # head
+        self.add_head_label = tk.Label(
+            self.buttons, text='头序列',
         )
-        self.add_head_entry.grid(row=1, column=0, columnspan=2)
+        self.add_head_label.grid(row=1, column=0)
+        self.add_head_entry = tk.Entry(
+            self.buttons, width=20, borderwidth=self._box_bd
+        )
+        self.add_head_entry.grid(row=1, column=1, columnspan=2)
         self.button_dict['添头'] = tk.Button(
-            self.buttons, text='添头',
+            self.buttons, text='添头', width=8,
             command=self.button_add_head
         )
-        self.button_dict['添头'].grid(row=1, column=2)
-        self.add_tail_entry = tk.Entry(
-            self.buttons, width=30
+        self.button_dict['添头'].grid(row=1, column=3)
+        # tail
+        self.add_tail_label = tk.Label(
+            self.buttons, text='尾序列',
         )
-        self.add_tail_entry.grid(row=1, column=3, columnspan=2)
+        self.add_tail_label.grid(row=2, column=0)
+        self.add_tail_entry = tk.Entry(
+            self.buttons, width=20, borderwidth=self._box_bd
+        )
+        self.add_tail_entry.grid(row=2, column=1, columnspan=2)
         self.button_dict['加尾'] = tk.Button(
-            self.buttons, text='加尾',
+            self.buttons, text='加尾', width=8,
             command=self.button_add_tail
         )
-        self.button_dict['加尾'].grid(row=1, column=5)
-
+        self.button_dict['加尾'].grid(row=2, column=3)
 
     def _textbox(self, label, width, textwrap, row, column):
         # make the text box with x and y scrollbar
@@ -115,7 +120,7 @@ class Tailorbird(tk.Frame):
         thelabel.grid(row=0, column=0)
         thetext = tk.Text(
             frame,
-            borderwidth=3,
+            borderwidth=self._box_bd,
             width=width,
             relief=tk.SUNKEN,
             wrap=textwrap
@@ -141,72 +146,66 @@ class Tailorbird(tk.Frame):
         thetext.configure(yscrollcommand=thescrolly.set)
         return thetext
 
-    def button_exchange(self):
-        # button exchange function
-        output_text = self.out_textbox.get(1.0, tk.END).rstrip()
-        self.in_textbox.delete(1.0, tk.END)
-        self.in_textbox.insert(1.0, output_text)
-
     def button_add_head(self):
         entry_text = self.add_head_entry.get()
-        input_text = self.in_textbox.get(
+        input_text = self.seq_text_box.get(
             1.0, tk.END
         ).rstrip().split('\n')
         output_text = [entry_text + x for x in input_text]
-        self.out_textbox.delete(1.0, tk.END)
-        self.out_textbox.insert(1.0, '\n'.join(output_text))
+        self.seq_text_box.delete(1.0, tk.END)
+        self.seq_text_box.insert(1.0, '\n'.join(output_text))
 
     def button_add_tail(self):
         entry_text = self.add_tail_entry.get()
-        input_text = self.in_textbox.get(
+        input_text = self.seq_text_box.get(
             1.0, tk.END
         ).rstrip().split('\n')
         output_text = [x + entry_text for x in input_text]
-        self.out_textbox.delete(1.0, tk.END)
-        self.out_textbox.insert(1.0, '\n'.join(output_text))
+        self.seq_text_box.delete(1.0, tk.END)
+        self.seq_text_box.insert(1.0, '\n'.join(output_text))
 
     def button_upper(self):
         # button upper function
-        output_text = self.in_textbox.get(
+        output_text = self.seq_text_box.get(
             1.0, tk.END
         ).rstrip().upper()
-        self.out_textbox.delete(1.0, tk.END)
-        self.out_textbox.insert(1.0, output_text)
+        self.seq_text_box.delete(1.0, tk.END)
+        self.seq_text_box.insert(1.0, output_text)
 
     def button_lower(self):
         # button lower function
-        output_text = self.in_textbox.get(
+        output_text = self.seq_text_box.get(
             1.0, tk.END
         ).rstrip().lower()
-        self.out_textbox.delete(1.0, tk.END)
-        self.out_textbox.insert(1.0, output_text)
+        self.seq_text_box.delete(1.0, tk.END)
+        self.seq_text_box.insert(1.0, output_text)
 
     def button_reverse(self):
         # button reverse function
-        input_text = self.in_textbox.get(
+        input_text = self.seq_text_box.get(
             1.0, tk.END
         ).rstrip().split('\n')
         output_text = [x[::-1] for x in input_text]
-        self.out_textbox.delete(1.0, tk.END)
-        self.out_textbox.insert(1.0, '\n'.join(output_text))
+        self.seq_text_box.delete(1.0, tk.END)
+        self.seq_text_box.insert(1.0, '\n'.join(output_text))
 
     def button_complement(self):
         # button complement function
-        input_text = self.in_textbox.get(
+        input_text = self.seq_text_box.get(
             1.0, tk.END
         ).rstrip().split('\n')
         output_text = [complement(x) for x in input_text]
-        self.out_textbox.delete(1.0, tk.END)
-        self.out_textbox.insert(1.0, '\n'.join(output_text))
+        self.seq_text_box.delete(1.0, tk.END)
+        self.seq_text_box.insert(1.0, '\n'.join(output_text))
 
     def button_reverse_complement(self):
         # button reverse complement function
-        input_text = self.in_textbox.get(
+        input_text = self.seq_text_box.get(
             1.0, tk.END
         ).rstrip().split('\n')
         output_text = [reverse_complement(x) for x in input_text]
-        self.out_textbox.delete(1.0, tk.END)
-        self.out_textbox.insert(1.0, '\n'.join(output_text))
+        self.seq_text_box.delete(1.0, tk.END)
+        self.seq_text_box.insert(1.0, '\n'.join(output_text))
 
 
 if __name__ == '__main__':
